5e640583a22a4598720f82ca874bd3ea3082d272
bhPark98 <138194791+bhPark98@users.noreply.github.com>
2024-05-01 18:31:41 +0900

    first commit

---
+++ src/main/java/com/springboot/leadingbooks/LeadingbooksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.leadingbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadingbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadingbooksApplication.class, args);
+	}
+
+}
