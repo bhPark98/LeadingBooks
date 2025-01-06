918721d9851dec97fdab1411a8839e11d2b1ae53
bhPark98 <beongho7730@naver.com>
2025-01-06 14:52:34 +0900

    5차 test

--- src/test/java/com/springboot/leadingbooks/LeadingbooksApplicationTests.java
+++ src/test/java/com/springboot/leadingbooks/LeadingbooksApplicationTests.java
@@ -1,14 +1,14 @@
-package com.springboot.leadingbooks;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LeadingbooksApplicationTests {
-
-	@Test
-	void contextLoads() {
-
-	}
-
-}
+//package com.springboot.leadingbooks;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LeadingbooksApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
